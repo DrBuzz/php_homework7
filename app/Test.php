6489c65de2f1410532a178311aaf74f8d6476451
DrBuzz <omenyzer@gmail.com>
2019-12-19 20:03:13 +0700

    phase 6

--- app/Test.php
+++ app/Test.php
@@ -4,8 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// php artisan db:seed --class=TestsTableSeeder
+
 class Test extends Model
 {
     protected $fillable = ['text','is_enabled'];
     protected $casts = ['is_enabled' => 'boolean'];
+
+    public static function findMax() {
+        $maxId = Test::all()->max('id');
+        Test::where('id', $maxId)->update(['text' => 'So what about Pepito?', 'is_enabled' => 1]);
+        return $maxId;
+    }
+    public static function lookForId($p){
+        $a = Test::where('id', $p)->get();
+        if (!$a) { return $a; }
+        else { return null; }
+    }
 }
